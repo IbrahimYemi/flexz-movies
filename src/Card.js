458cdd60926fbe215db31b4d7239f76d6e0cb89e
IbrahimYemi <sharafadeen.might@gmail.com>
2022-07-14 02:34:33 +0100

    3 files modified

--- src/Card.js
+++ src/Card.js
@@ -1,28 +1,15 @@
 import React from 'react';
+import Movies from './Movies';
+import Footers from './Footers';
+import Developer from './Developer';
 
 export default function Card() {
-  const [movieData, setMovieData] = React.useState([]);
 
-  const API_KEY = 'api_key=282292c1d01116b3d3ccf1c8c40cf303';
-  const BASE_URL = 'https://api.themoviedb.org/3';
-  const API_URL =
-    BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
-  const searchURL = BASE_URL + '/search/movie?' + API_KEY;
 
-  React.useEffect(function () {
-    fetch(API_URL)
-      .then((res) => res.json())
-      .then((data) => setMovieData(data.results));
-  }, []);
-  console.log(movieData);
   return (
-    <>
-      <div className="card-holder">
-        {movieData.lenght > 0 &&
-          movieData.map((movies) => {
-            <Movies key={movies.id} data={movies} />;
-          })}
-      </div>
-    </>
+    <div>
+      <Movies />
+      {/* <Developer /> */}
+    </div>
   );
 }
--- src/Movies.js
+++ src/Movies.js
@@ -1,21 +1,47 @@
 import React from 'react';
-import Card from './Card';
 
-export default function Movies(title, poster_path, vote_average, release_date) {
-  const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-  return (
-    <div className="card-container">
-      <div className="card">
-        <img src={data.poster_path} alt={data.title} />
-        <div className="title-rating">
-          <h3 className="title">{data.title}</h3>
-          <span className="rating">{data.vote_average}</span>
+export default function Movies() {
+  const [movieData, setMovieData] = React.useState([]);
+
+  const API_KEY = 'api_key=282292c1d01116b3d3ccf1c8c40cf303';
+  const BASE_URL = 'https://api.themoviedb.org/3';
+  const API_URL =
+    BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+  const searchURL = BASE_URL + '/search/movie?' + API_KEY;
+
+  React.useEffect(function () {
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => setMovieData(data.results));
+  }, []);
+  console.log(movieData);
+
+  const movie = ['1', '2', '3'];
+  const cards = movieData.map((item) => {
+    return (
+      <div className="card-container">
+        <div className="card">
+          <img src={movieData.poster_path} alt={movieData.title} />
+          <div className="title-rating">
+            <h3 className="title">{movieData.title}</h3>
+            <span className="rating">{movieData.vote_average}</span>
+          </div>
+        </div>
+        <div className="release">{movieData.release_date}</div>
+        <div className="review">
+          <button>Trailer</button>
         </div>
       </div>
-      <div className="release">{data.release_date}</div>
-      <div className="review">
-        <button>Trailer</button>
-      </div>
-    </div>
-  );
-}
+    )
+  });
+  return(
+    <div>{cards}</div>
+  )
+};
+
+
+// export default function Movies(title, poster_path, vote_average, release_date) {
+//   const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+ 
+//   );
+// }
