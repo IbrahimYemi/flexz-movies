3de7b929b8d0d6f4e32bb21365037c0be9807597
IbrahimYemi <sharafadeen.might@gmail.com>
2022-07-14 01:03:00 +0100

    1 file modified apibkey

--- src/Card.js
+++ src/Card.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
 export default function Card() {
-  // const cards = data.map((item) => {
-  //   return (
-  //     <div key={item.id} className={props.darkMode ? 'darkcard' : 'card'}>
-  //       <h2>{item.title}</h2>
-  //       <p>{item.description}</p>
-  //     </div>
-  //   );
-  // });
+  const [movieData, setMovieData] = React.useState([]);
+
+  const API_KEY = 'api_key=282292c1d01116b3d3ccf1c8c40cf303';
+  const BASE_URL = 'https://api.themoviedb.org/3';
+  const API_URL =
+    BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
+  const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+  const searchURL = BASE_URL + '/search/movie?' + API_KEY;
+
+  React.useEffect(function () {
+    console.log('Effect ran');
+    fetch(API_URL)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  });
 
   const cardcontent = (
     <div className="card-container">
