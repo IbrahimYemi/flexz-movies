1e1bec7f369803f291058feafe3de906c7615b52
IbrahimYemi <sharafadeen.might@gmail.com>
2022-07-14 01:35:14 +0100

    2 files modified

--- src/Card.js
+++ src/Card.js
@@ -7,7 +7,6 @@ export default function Card() {
   const BASE_URL = 'https://api.themoviedb.org/3';
   const API_URL =
     BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
-  const IMG_URL = 'https://image.tmdb.org/t/p/w500';
   const searchURL = BASE_URL + '/search/movie?' + API_KEY;
 
   React.useEffect(function () {
@@ -16,29 +15,11 @@ export default function Card() {
       .then((data) => setMovieData(data.results));
   }, []);
   console.log(movieData)
-  // const cardcontent = (
-  //   <div className="card-container">
-  //     <div className="card">
-  //       <img
-  //         src="https://media.istockphoto.com/photos/the-musicians-were-playing-rock-music-on-stage-there-was-an-audience-picture-id1319479588?b=1&k=20&m=1319479588&s=170667a&w=0&h=bunblYyTDA_vnXu-nY4x4oa7ke6aiiZKntZ5mfr-4aM="
-  //         alt=""
-  //       />
-  //       <div className="title-rating">
-  //         <h3 className="title">film title</h3>
-  //         <span className="rating">7.8</span>
-  //       </div>
-  //     </div>
-  //     <div className="review">
-  //       <button>Trailer</button>
-  //     </div>
-  //   </div>
-  // );
-
   return (
     <>
       <div className="card-holder">
         {movieData.lenght > 0 && movieData.map(movies=>{
-          <Movies key={movies.id} data={movie}/>
+          <Movies key={movies.id} data={movies}/>
         })}
       </div>
     </>
--- src/Movies.js
+++ src/Movies.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
-export default function Movies() {
+export default function Movies(title, poster_path, vote_average) {
+  const IMG_URL = 'https://image.tmdb.org/t/p/w500';
   return (
     <div className="card-container">
       <div className="card">
-        <img
-          src={movies.backdrop_path}
-          alt={movies.title}
-        />
+        <img src={movies.poster_path} alt={movies.title} />
         <div className="title-rating">
           <h3 className="title">{movies.title}</h3>
           <span className="rating">{movies.vote_average}</span>
